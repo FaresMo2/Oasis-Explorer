9bc9323a5034e4af3fd4e4230fc938b31756def6
Fares Mohamed <faresmohamed698@gmail.com>
2024-09-13 23:21:08 +0300

    "upload files"

---
+++ app/account/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Guest Area",
+};
+const Page = () => {
+  return (
+    <div>
+      <h1>Your Account</h1>
+
+      <button active={true} className="input">
+        Fares Now
+      </button>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/cabins/page.js
@@ -0,0 +1,16 @@
+import Counter from "@/app/_components/Counter";
+// @/app => Next.js Automatically Support import Aliases
+
+export const metadata = {
+  title: "Cabins",
+};
+
+const Page = async () => {
+  return (
+    <div>
+      <h1>All Cabins</h1>
+    </div>
+  );
+};
+
+export default Page;
